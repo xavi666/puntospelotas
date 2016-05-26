e1835d41798b2b13cc21ce05cfc78270b4152695
Xavier Garces <xgarcesmarti@gmail.com>
2016-05-26 14:30:32 +0100

    user api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,59 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :except => [:create]
 
   def index
     @users = User.all
   end
 
-  def update
-    @user.assign_attributes(@json['user'])
-    @user.save
+  def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.json { render :show, status: :created }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.json { render :show, status: :ok }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params
+        .require(:user)
+        .permit(:email, :password, :password_confirmation)
+    end
 end
