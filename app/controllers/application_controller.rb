c56b5c1f0837bfef78185a7138173e95d001eee5
= <xavier@flowlens.com>
2016-06-01 15:28:33 +0100

    configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -11,20 +10,26 @@ class ApplicationController < ActionController::Base
     render layout: layout_name
   end
 
-  private
+  
 
-  def layout_name
-    if params[:layout] == 0
-      false
-    else
-      'application'
-    end
-  end
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   protected
-
+  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :push_comunio])
+     devise_parameter_sanitizer.for(:account_update) << :name
   end
+
+
+  private
+
+    def layout_name
+      if params[:layout] == 0
+        false
+      else
+        'application'
+      end
+    end
 
 end
