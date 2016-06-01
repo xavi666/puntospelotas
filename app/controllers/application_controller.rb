bd53712942e84658e9679bc5737151a8e8fe8812
= <xavier@flowlens.com>
2016-06-01 13:43:35 +0100

    emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -18,6 +19,12 @@ class ApplicationController < ActionController::Base
     else
       'application'
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :push_comunio])
   end
 
 end
