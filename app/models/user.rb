9b86a7aefff5d50d539e1a2cbb2ab53404d42a7d
= <xavier@flowlens.com>
2016-06-02 12:04:45 +0100

    email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
           :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
+  after_create :send_email
+
+  def send_email
+    UserMailer.welcome_email(self).deliver
+  end
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,11 +74,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSSWORD']
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
