af13d6b51daffe2f8adca76bdda3d19479cd5106
Xavi <=>
2016-06-12 20:23:05 +0100

    bundle update & rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606153556) do
+ActiveRecord::Schema.define(version: 20160610143038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160606153556) do
     t.string   "stage"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "points",               default: false
     t.string   "comunio_url"
     t.boolean  "comunio_points",       default: false
     t.json     "comunio_points_value"
@@ -64,13 +65,13 @@ ActiveRecord::Schema.define(version: 20160606153556) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider",                              null: false
-    t.string   "uid",                    default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "provider",                            null: false
+    t.string   "uid",                    default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -86,7 +87,7 @@ ActiveRecord::Schema.define(version: 20160606153556) do
     t.text     "tokens"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "push_comunio",           default: true
+    t.string   "ionic_push_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
